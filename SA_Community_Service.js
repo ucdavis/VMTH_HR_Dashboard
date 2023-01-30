e0cdd9c0b9c7fe85b3640e56b7a4525e9b2b7004
Garrett Hill <gehill123@gmail.com>
2023-01-30 12:51:53 -0800

    Add files via upload

--- SA_Community_Service.js
+++ SA_Community_Service.js
@@ -80,7 +80,7 @@ svg.append("text")
         .attr("x", 0)
         .attr("y", -20)
         .attr("text-anchor", "left")
-        .text("November 2022");
+        .text("December 2022");
 
   
 
@@ -206,7 +206,7 @@ svg.append("text")
         .attr("x", 0)
         .attr("y", -20)
         .attr("text-anchor", "left")
-        .text("November 2022");
+        .text("December 2022");
 
   
 
@@ -327,7 +327,7 @@ svg.append("text")
         .attr("x", 0)
         .attr("y", -20)
         .attr("text-anchor", "left")
-        .text("November 2022");
+        .text("December 2022");
 
   
 
@@ -656,7 +656,7 @@ svg.append("text")
         .attr("x", 0)
         .attr("y", -40)
         .attr("text-anchor", "left")
-        .text("November 2022");
+        .text("December 2022");
 
 
 
